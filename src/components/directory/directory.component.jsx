eb254a9c61eb320ba04759dec14d6c1b3ae7a952
unknown <duckhang2210@gmail.com>
2021-10-18 23:52:14 -0500

    update shop assets

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -10,33 +10,33 @@ class Directory extends Component {
         this.state = {
             sections: [
                 {
-                    title: 'hats',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    title: 'floral',
+                    imageUrl: 'https://images.pexels.com/photos/4041392/pexels-photo-4041392.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
                     id: 1,
-                    linkUrl: 'shop/hats'
+                    linkUrl: 'shop/floral'
                   },
                   {
-                    title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                    title: 'fresh',
+                    imageUrl: 'https://images.pexels.com/photos/6794057/pexels-photo-6794057.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
                     id: 2,
-                    linkUrl: 'shop/jackets'
+                    linkUrl: 'shop/fresh'
                   },
                   {
-                    title: 'sneakers',
-                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                    title: 'sweet',
+                    imageUrl: 'https://images.pexels.com/photos/6037893/pexels-photo-6037893.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
                     id: 3,
-                    linkUrl: 'shop/sneakers'
+                    linkUrl: 'shop/sweet'
                   },
                   {
                     title: 'womens',
-                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                    imageUrl: 'https://images.pexels.com/photos/4659794/pexels-photo-4659794.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
                     size: 'large',
                     id: 4,
                     linkUrl: 'shop/womens'
                   },
                   {
                     title: 'mens',
-                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                    imageUrl: 'https://images.pexels.com/photos/1895015/pexels-photo-1895015.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
                     size: 'large',
                     id: 5,
                     linkUrl: 'shop/mens'
