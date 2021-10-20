48aff8af2a8a41cb4309ff4f9251524405bf5f27
unknown <duckhang2210@gmail.com>
2021-10-20 17:58:17 -0500

    add firebase utils

--- src/components/signin-signup/signin-signup.component.jsx
+++ src/components/signin-signup/signin-signup.component.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './signin-signup.style.scss'
 import SignIn from '../sign-in/sign-in.component';
+import SignUp from '../sign-up/sign-up.component';
 
 const SignInAndSignUp = () => (
     <div className='sign-in-and-sign-up'>
         <SignIn />
+        <SignUp />
     </div>
 )
 
